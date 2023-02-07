10e63d2a2c3ad4dd0a06bf2638a6f80e66913923
Mohammad Shahadad Hossain <shahadadrohid@gmail.com>
2023-02-07 15:50:10 +0600

    Booking route with query

--- index.js
+++ index.js
@@ -22,12 +22,25 @@ async function run() {
     try {
         await client.connect();
         const servicesCollection = client.db('doctors_portal').collection('services');
+        const bookingCollection = client.db('doctors_portal').collection('bookings');
 
         app.get('/service', async (req, res) => {
             const query = {};
             const cursor = servicesCollection.find(query);
             const services = await cursor.toArray();
             res.send(services);
+        })
+        app.post('/booking', async (req, res) => {
+            const booking = req.body;
+            const query = { treatment: booking.treatment, date: booking.date, patient: booking.patient }
+            const exixts = await bookingCollection.findOne(query)
+            if (exixts) {
+                return res.send({ success: false, booking: exixts });
+            }
+            else {
+                const result = await bookingCollection.insertOne(booking);
+                res.send({ success: true, result });
+            }
         })
     }
     finally {
